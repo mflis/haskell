469114cec21322771f1e66d0c324793329977867
marcin <marcin.flis.95@gmail.com>
2016-05-18 00:35:20 +0200

    night

--- warcaby.hs
+++ warcaby.hs
@@ -1,25 +1,27 @@
 --import Data.Map
 import Debug.Trace
+import Data.Maybe
 
 
 
 
 data Field = White | WhiteQueen | Black | BlackQueen | Empty deriving Show
 
-data Board = Board [[Field]] deriving Show
+data Board = Board [[ Maybe Field]] deriving Show
 
 readField char 
- | char == 'b' = Black
- | char == 'B' = BlackQueen
- | char == 'w' = White
- | char == 'W' = WhiteQueen
- | char == '.' = Empty
+ | char == 'b' = Just Black
+ | char == 'B' = Just BlackQueen
+ | char == 'w' = Just White
+ | char == 'W' =  Just WhiteQueen
+ | char == '.' =  Just Empty
  | otherwise = error "parse error"
 
 mapListOfFields singleLineString = map readField singleLineString
 
 makeBoardFromString :: String -> Board
-makeBoardFromString stringRepresentation = Board $ map mapListOfFields (words stringRepresentation) 
+makeBoardFromString stringRepresentation = 
+ Board $ map mapListOfFields (words stringRepresentation) 
 
 --indicies  = [[x,y] | x <- "abcdefgh", y <- "12345678"]
 --filterNewLines string = Prelude.filter (/='\n') string 
@@ -39,14 +41,14 @@ getLegalPositions rows = [(c, d) | c <- [1..8], d <- rows, or [and [even c, even
 
 --	USING !! OPERATOR LISTS ARE 0 - INDEXED
 
-getFigureAtPosition :: (Int,Int) -> Board -> Field
-getFigureAtPosition position@(x,y) (Board listOfLists) = 
- (listOfLists !! (y-1)) !! (x-1)
+--getFigureAtPosition :: (Int,Int) -> Board -> Maybe Field
+getFigureAtPosition position@(x,y) (Board listOfLists) 
+ | 0 < x &&  x <= length listOfLists && 0 < y && y <= length listOfLists  =  (listOfLists !! (y-1)) !! (x-1)
+ | otherwise = Nothing
 
 
 
-
-changeFigureInLine :: Int ->  [Field] -> Field -> [Field]
+--changeFigureInLine :: Int ->  [Field] -> Field -> [Field]
 changeFigureInLine positionInLine line newFigure =  
  let (firstPart,secondPart) = splitAt positionInLine  line   
  in   (trimLastElement firstPart) ++ [newFigure] ++ secondPart
@@ -57,20 +59,24 @@ trimLastElement list = init list
 
 --changeFigureInPosition :: (Int,Int) -> Field -> Board -> Board
 changeFigureInPosition  position@(x,y) newFigure (Board listOfLists) = 
- let (firstPart,secondPart) = splitAt y listOfLists   in   
- trace ("first: " ++ (show $ trimLastElement firstPart) ++ "\n changed: " ++ (show changedLine)  ++ "\n second: " ++ (show secondPart) ) $ 
+ let (firstPart,secondPart) = splitAt y listOfLists   in    
  Board((trimLastElement firstPart) ++ [changedLine]  ++ secondPart)
  where changedLine =  changeFigureInLine x (listOfLists !! (y-1)) newFigure
 
 
 changeFigureInPos  position@(x,y) newFigure ( listOfLists) = 
  let (firstPart,secondPart) = splitAt y listOfLists   in   
- trace ("first: " ++ (show $ trimLastElement firstPart) ++ "\n changed: " ++ (show changedLine)  ++ "\n second: " ++ (show secondPart) ) $ 
- (( trimLastElement firstPart) ++ [changedLine]  ++ secondPart)
- where changedLine =  trace ("line to change: " ++ show(listOfLists !! (y -1))) $ changeFigureInLine x (listOfLists !! (y-1)) newFigure
+ (trimLastElement firstPart) ++ [changedLine]  ++ secondPart
+ where changedLine = changeFigureInLine x (listOfLists !! (y-1)) newFigure
 
 
 moveFigure oldPosition newPosition board = 
- let removedOldPosition = changeFigureInPosition oldPosition Empty board
- in  trace ("after removing old: " ++ show removedOldPosition) $ changeFigureInPosition newPosition oldFigure removedOldPosition
+ let removedOldPosition = changeFigureInPosition oldPosition (Just Empty) board
+ in  changeFigureInPosition newPosition oldFigure removedOldPosition
  where oldFigure = getFigureAtPosition oldPosition  board
+
+
+
+--getListOfLegalMovesFromPosition :: (Int,Int) -> Board -> [(Int,Int)]
+
+
