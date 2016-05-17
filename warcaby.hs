8b2ae796a005e3811a3a7bc6472b4cdbb0d97144
marcin <marcin.flis.95@gmail.com>
2016-05-17 22:00:02 +0200

    aftef debug of wrong positions

--- warcaby.hs
+++ warcaby.hs
@@ -1,4 +1,8 @@
 --import Data.Map
+import Debug.Trace
+
+
+
 
 data Field = White | WhiteQueen | Black | BlackQueen | Empty deriving Show
 
@@ -28,28 +32,45 @@ makeBoardFromString stringRepresentation = Board $ map mapListOfFields (words st
 initialBoardStr = ".b.b.b.b\nb.b.b.b.\n.b.b.b.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
 
 
-init2Board = makeBoardFromString ".b.b\n.b.b\n.b.b"
+init2Board = makeBoardFromString ".b.b\nb.b.\n.b.b"
 
 getLegalPositions rows = [(c, d) | c <- [1..8], d <- rows, or [and [even c, even d], and [odd c, odd d]] ]
 
---data Position = Position { x:: Int,y :: Int} deriving (Show)   
+
+--	USING !! OPERATOR LISTS ARE 0 - INDEXED
 
 getFigureAtPosition :: (Int,Int) -> Board -> Field
 getFigureAtPosition position@(x,y) (Board listOfLists) = 
- (listOfLists !! y) !! x
+ (listOfLists !! (y-1)) !! (x-1)
+
+
 
 
 changeFigureInLine :: Int ->  [Field] -> Field -> [Field]
 changeFigureInLine positionInLine line newFigure =  
- let (firstPart,secondPart) = splitAt positionInLine line   in   (init firstPart) ++ [newFigure] ++ secondPart
+ let (firstPart,secondPart) = splitAt positionInLine  line   
+ in   (trimLastElement firstPart) ++ [newFigure] ++ secondPart
 
+--workaround because init on empty list throws exception
+trimLastElement [] = []
+trimLastElement list = init list 
 
-changeFigureInPosition :: (Int,Int) -> Field -> Board -> Board
+--changeFigureInPosition :: (Int,Int) -> Field -> Board -> Board
 changeFigureInPosition  position@(x,y) newFigure (Board listOfLists) = 
- let (firstPart,secondPart) = splitAt y listOfLists   in   Board((init firstPart) ++ [changedLine]  ++ secondPart)
- where changedLine =  changeFigureInLine x (listOfLists !! y) newFigure
+ let (firstPart,secondPart) = splitAt y listOfLists   in   
+ trace ("first: " ++ (show $ trimLastElement firstPart) ++ "\n changed: " ++ (show changedLine)  ++ "\n second: " ++ (show secondPart) ) $ 
+ Board((trimLastElement firstPart) ++ [changedLine]  ++ secondPart)
+ where changedLine =  changeFigureInLine x (listOfLists !! (y-1)) newFigure
 
-moveFigure oldPosition newPosition board = changeFigureInPosition oldPosition Empty board
- --let removedOldPosition = changeFigureInPosition oldPosition Empty board
- --in changeFigureInPosition newPosition oldFigure removedOldPosition
- --where oldFigure = getFigureAtPosition oldPosition  board
+
+changeFigureInPos  position@(x,y) newFigure ( listOfLists) = 
+ let (firstPart,secondPart) = splitAt y listOfLists   in   
+ trace ("first: " ++ (show $ trimLastElement firstPart) ++ "\n changed: " ++ (show changedLine)  ++ "\n second: " ++ (show secondPart) ) $ 
+ (( trimLastElement firstPart) ++ [changedLine]  ++ secondPart)
+ where changedLine =  trace ("line to change: " ++ show(listOfLists !! (y -1))) $ changeFigureInLine x (listOfLists !! (y-1)) newFigure
+
+
+moveFigure oldPosition newPosition board = 
+ let removedOldPosition = changeFigureInPosition oldPosition Empty board
+ in  trace ("after removing old: " ++ show removedOldPosition) $ changeFigureInPosition newPosition oldFigure removedOldPosition
+ where oldFigure = getFigureAtPosition oldPosition  board
